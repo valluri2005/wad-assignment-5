3c95174f1a2df12fa7cafe8dff0cb23dcbf70544
valluri2005 <valluri2@gitam.in>
2026-04-15 14:50:51 +0530

    Fetch courseAnalytics.json and analyze student data

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-fetch('students.json')
+fetch("courseAnalytics.json")
   .then(response => response.json())
   .then(data => {
-    const filteredStudents = data.filter(student => student.cgpa > 8.0);
 
-    console.log("Students with CGPA > 8.0:");
-    filteredStudents.forEach(student => {
-      console.log(`${student.name} (${student.department}) - CGPA: ${student.cgpa}`);
+    console.log("Students at Academic Risk:");
+
+    let totalInternal = 0;
+    let totalAssignments = 0;
+    let assignmentCount = 0;
+
+    data.students.forEach(student => {
+
+      let reasons = [];
+
+      if (student.attendance_percentage < 75)
+        reasons.push("Low Attendance");
+
+      if (student.internal_marks < 40)
+        reasons.push("Low Internal Marks");
+
+      if (reasons.length > 0) {
+        console.log(`${student.name} → ${reasons.join(", ")}`);
+      }
+
+      totalInternal += student.internal_marks;
+
+      student.assignment_scores.forEach(score => {
+        totalAssignments += score;
+        assignmentCount++;
+      });
     });
+
+    const avgInternal = totalInternal / data.students.length;
+    const avgAssignments = totalAssignments / assignmentCount;
+
+    console.log("\nClass Average:");
+    console.log("Internal Marks:", avgInternal.toFixed(2));
+    console.log("Assignment Scores:", avgAssignments.toFixed(2));
   })
-  .catch(error => console.error("Error:", error));
+  .catch(err => console.log(err));
